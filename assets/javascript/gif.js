977087322584bce6108a7df08c51274f7ba59435
koa2019 <no-reply>
2019-07-21 14:16:53 -0700

    added bootstrap containers, rows & columns to index. created loop to create buttons & append to html for cartoons array. Added a user input form on index.html

--- assets/javascript/gif.js
+++ assets/javascript/gif.js
@@ -1,11 +1,29 @@
+// global variables
 var apiKey = 'WJ8rJf7UKPx63l6ga90zUlJpio0LezKb';
 var queryURL1 = "http://api.giphy.com/v1/gifs/search?q=" + search + '&api_key=' + apiKey;
 var queryURL = "https://api.giphy.com/v1/gifs/trending?api_key=WJ8rJf7UKPx63l6ga90zUlJpio0LezKb";
 var search = "";
+var cartoons = ['Homer Simpson', 'Peter Griffin', 'Sailor Moon', 'Pinky & the Brain'];
+var index;
 
-$.ajax({
-    url: queryURL,
-    method: 'GET'
-}).then(function(response) {
-    console.log(response);
+$(document).ready(function() {
+
+    for (var x = 0; x < cartoons.length; x++) {
+        console.log(x)
+        index = cartoons[x];
+        var button = $('<button>') //create button
+            .addClass('cartoon')
+            .attr('name', index)
+            .text(index);
+        $('.buttonContainer').append(button); //create button & add to page
+    }
+
+
+    // $.ajax({
+    //     url: queryURL,
+    //     method: 'GET'
+    // }).then(function(response) {
+    //     console.log(response);
+    // })
+
 })
